aca45181c98322d0127753a79eb676f2169f9d9a
CurtisFlaming <flamingc@ksu.edu>
2019-09-20 15:22:09 -0500

    tritops

---
+++ Menu/Sides/Triceritots.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class Triceritots : Side
+    {
+
+        public override List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Potato", "Salt", "Vegetable Oil"};
+                return ingredients;
+            }
+        }
+
+
+        private Size size;
+        public override Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 590;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 410;
+                        break;
+                    case Size.Small:
+                        Price = .99;
+                        Calories = 352;
+                        break;
+                }
+
+            }
+        }
+
+        public Triceritots()
+        {
+            //this.Price = 0.99;
+            //this.Calories = 352;
+            this.Size = Size.Small;
+        }
+
+    }
+}
